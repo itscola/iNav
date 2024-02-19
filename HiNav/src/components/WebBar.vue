9c43101aed60426bf347e5f1f761dddef529b425
itscola <white_cola@foxmail.com>
2024-02-19 16:17:56 +0800

    'Add new Link' dialog menu.

--- HiNav/src/components/WebBar.vue
+++ HiNav/src/components/WebBar.vue
@@ -6,38 +6,28 @@
       style="font-size: 24px; color: whitesmoke; margin-left: 15px; font-weight: 1000;">iNav</v-app-bar-title>
 
     <template v-slot:append>
-      <!-- 登录对话框的触发按钮 -->
       <v-btn icon style="color: whitesmoke;" @click="loginDialog = true"><v-icon>mdi-account</v-icon></v-btn>
-
       <v-btn icon style="color: whitesmoke;" @click="dialog = true"><v-icon>mdi-magnify</v-icon></v-btn>
-
-      <!-- <v-btn icon style="color: whitesmoke;"><v-icon>mdi-dots-vertical</v-icon></v-btn> -->
-
-
       <v-menu>
         <template v-slot:activator="{ props }">
           <v-btn icon v-bind="props" style="color: whitesmoke;"><v-icon>mdi-dots-vertical</v-icon></v-btn>
         </template>
         <v-list style="padding-top: 0; padding-bottom: 0;">
-
-          <v-list-item @click="someAction1" class="icon-button">
+          <v-list-item @click="addAction" class="icon-button">
             <v-icon color="rgb(122,122,122)">mdi-plus</v-icon>
           </v-list-item>
-          <v-list-item @click="someAction2" class="icon-button">
+          <v-list-item @click="removeAction" class="icon-button">
             <v-icon color="rgb(122,122,122)">mdi-minus</v-icon>
           </v-list-item>
-          <v-list-item @click="someAction3" class="icon-button">
+          <v-list-item @click="categoryAction3" class="icon-button">
             <v-icon color="rgb(122,122,122)">mdi-view-list</v-icon>
           </v-list-item>
-
         </v-list>
       </v-menu>
-
     </template>
   </v-app-bar>
 
-  <!-- 登录对话框 -->
-  <v-dialog v-model="loginDialog" hide-overlay persistent max-width="410px">
+  <v-dialog v-model="loginDialog" hide-overlay max-width="410px">
     <v-card class="elevation-12">
       <v-toolbar dark color="primary">
         <v-toolbar-title>Login</v-toolbar-title>
@@ -50,11 +40,6 @@
         <v-form @submit.prevent="login">
           <v-text-field v-model="email" label="Email" outlined required></v-text-field>
           <v-text-field v-model="password" label="Password" outlined type="password" required></v-text-field>
-
-          <!-- <v-btn color="primary" dark type="submit">Sign in</v-btn>
-            <p style="color: rgb(216, 27, 96); text-decoration: underline; cursor: pointer; margin-top: 8px; margin-left: 2px;" @click="goToSignUpPage">Sign up ?</p>
-         -->
-
           <v-row justify="space-between" align="center" no-gutters class="mx-0">
             <v-col cols="auto">
               <v-btn color="primary" dark type="submit">Sign in</v-btn>
@@ -64,7 +49,6 @@
                 @click="goToSignUpPage">Sign up ?</p>
             </v-col>
           </v-row>
-
         </v-form>
       </v-card-text>
     </v-card>
@@ -78,6 +62,45 @@
       </v-card-text>
     </v-card>
   </v-dialog>
+
+  <v-dialog v-model="addDialog" hide-overlay max-width="500px">
+  <v-card>
+    <v-toolbar dark color="primary">
+      <v-toolbar-title>Add New Link</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon @click="addDialog = false">
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-card-text>
+      <v-form>
+        <v-text-field v-model="newLink.url" label="URL" outlined required></v-text-field>
+        <v-text-field v-model="newLink.name" label="Link Name" outlined required></v-text-field>
+        <v-select v-model="newLink.category" :items="categories" label="Category" outlined required></v-select>
+        <v-text-field v-model="newLink.weight" label="Weight" type="number" outlined required></v-text-field>
+        <v-switch v-model="newLink.isPrivate" label="Private?" color="primary" outlined></v-switch>
+        <v-textarea v-model="newLink.description" label="Description" outlined></v-textarea>
+        <v-row class="pa-2">
+
+          <v-col cols="3" style="padding-left: 4px;">
+            <v-btn color="primary" dark @click="addLink">Add</v-btn>
+          </v-col>
+
+          <v-col cols="3" offset="4" style="padding-right: 20px;">
+            <v-btn color="green darken-1" text @click="analyzeLink">Analyze</v-btn>
+          </v-col>
+
+          <v-col cols="2" style="padding-left: 5px; padding-right: 3px;">
+            <v-btn color="blue darken-1" text @click="resetForm">Reset</v-btn>
+          </v-col>
+          
+
+
+        </v-row>
+      </v-form>
+    </v-card-text>
+  </v-card>
+</v-dialog>
 
   <v-navigation-drawer v-model="drawer" app right clipped :width="drawerHover ? 180 : 60" @mouseover="drawerHover = true"
     @mouseleave="drawerHover = false">
@@ -98,24 +121,32 @@ export default {
       drawer: true,
       drawerHover: false,
       dialog: false,
-      loginDialog: false, // 控制登录对话框的显示
-      email: '', // 登录表单的邮箱字段
-      password: '', // 登录表单的密码字段
+      loginDialog: false,
+      email: '',
+      password: '',
       searchText: '',
+      addDialog: false,
+      newLink: {
+        url: '',
+        name: '',
+        category: '',
+        weight: 0,
+        isPrivate: false,
+        description: '',
+      },
+      categories: ['Category 1', 'Category 2', 'Category 3'],
+      
     };
   },
   methods: {
     login() {
-      // 这里实现登录逻辑
       console.log('Email:', this.email, 'Password:', this.password);
-      // 登录成功后关闭对话框
       this.loginDialog = false;
     },
     goToSignUpPage() {
-      // 导航到注册页面的逻辑
+
     },
     goToAccountPage() {
-      // 导航到账户页面的逻辑
       this.$router.push('/login');
     },
     goToMainPage() {
@@ -126,7 +157,28 @@ export default {
     },
     goToGithubPage() {
       window.open('https://github.com/itscola/iNav', '_blank');
-    }
+    },
+    addAction() {
+      this.addDialog = true;
+    },
+    addLink() {
+      console.log('Adding link:', this.newLink);
+      this.addDialog = false;
+      this.resetForm();
+    },
+    analyzeLink() {
+      console.log('Analyzing link:', this.newLink.url);
+    },
+    resetForm() {
+      this.newLink = {
+        url: '',
+        name: '',
+        category: '',
+        weight: 0,
+        isPrivate: false,
+        description: '',
+      };
+    },
   }
 }
 </script>
